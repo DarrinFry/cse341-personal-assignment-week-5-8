fc5778821a15996bab388e7cdda437f79b89fb99
Darrin Fry <productionstagemanager@gmail.com>
2022-06-02 19:23:30 -0600

    attempting to do authO oAuth stuff

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const swaggerDocument = require('./swagger-output.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const { auth } = require('express-openid-connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -57,4 +58,23 @@ mongodb.initDb((err, mongodb) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+});
+
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
