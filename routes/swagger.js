6f227509ef60d73112e318f9ebedc8fe8c5b64ef
Darrin Fry <productionstagemanager@gmail.com>
2022-06-09 23:19:51 -0600

    issues reverting

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,12 +4,9 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of d68e0c2 (Attempts to implement Auth0 api-docs)
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', requiresAuth(), swaggerUi.serve);
+router.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
