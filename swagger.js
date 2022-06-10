e6058590aa1b7a12daa358048020558cebdf787b
Darrin Fry <productionstagemanager@gmail.com>
2022-06-10 00:14:32 -0600

    again, more button issues.

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Darrin Fry Personal Assignment API Week 5-8',
-    description: `Custom Projects API  Please watch this video to understand the end goal from the project and the reason for these databases and APIs: https://youtu.be/u_Lqnaghzok  ---- <form action='${process.env.BASE_URL}/logout'> <input type='submit' value= 'Logout' /> </form> ---- `,
+    description: `Custom Projects API  Please watch this video to understand the end goal from the project and the reason for these databases and APIs: https://youtu.be/u_Lqnaghzok  ---- <input type='button' onclick='location.href="${process.env.BASE_URL}/logout";' value='Logout' /> ---- `,
   },
   host: 'd-fry-cse341-personal.herokuapp.com',
   schemes: ['https'],
